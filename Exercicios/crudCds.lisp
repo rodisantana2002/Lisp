3d093eef77c001d340465705e8ee9bf21ee3e6fa
Rodolfo Santana <rodolfosmac@Rodolfos-Mac.local>
2018-01-27 00:08:48 -0200

    fg

--- Exercicios/crudCds.lisp
+++ Exercicios/crudCds.lisp
@@ -16,17 +16,18 @@
   (force-output *query-io*)
   (read-line *query-io*))
 
+
 (defun prompt-for-cd ()
-  	  (make-cd
-	   (prompt-read "Title")
-	   (prompt-read "Artist")
-	   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
-	   (y-or-n-p "Ripped [y/n]: ")))
+      (make-cd
+       (prompt-read "Title")
+       (prompt-read "Artist")
+       (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+       (y-or-n-p "Ripped [y/n]: ")))
 
 (defun add-cds ()
   (loop (add-record (prompt-for-cd))
-(if (not (y-or-n-p "Another? [y/n]: ")) (return))))
-
+   (if (not (y-or-n-p "Another? [y/n]: "))
+    (return))))
 
 (defun save-db (filename)
   (with-open-file (out filename
@@ -51,7 +52,7 @@
       (and
         (if title    (equal (getf cd :title)  title) t)
         (if artist   (equal (getf cd :artist) artist) t)
-        (if rating   (equal (getf cd :rating) rating) t)
+        (if rating   (>=    (getf cd :rating) rating) t)
         (if ripped-p (equal (getf cd :ripped) ripped) t))))
 
 (defun update (selector-fn &key title artist rating (ripped nil ripped-p))
--- Exercicios/exemplos.lisp
+++ Exercicios/exemplos.lisp
@@ -1,3 +1,2 @@
 (defclass box ()
-  ((nome :accessor box-nome :initarg :nome :initform (error "Must supply a customer name."))
-  )
+  ((nome :accessor box-nome :initarg :nome :initform (error "Must supply a customer name."))))
